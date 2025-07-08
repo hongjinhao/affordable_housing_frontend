79a6e964d38edcb49ced97f43f05865e9f441bf6
Jin Hao <jinhaoho@usc.edu>
2025-07-08 13:39:39 -0700

    use relative URL

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ form.addEventListener('submit', async (event) => {
     };
 
     try {
-        const response = await fetch(`${apiBaseUrl}/predict`, {
+        const response = await fetch(`/predict`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
